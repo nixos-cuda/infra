fc40353a0c0d8fc5582767d2bdb261a2278d698b
Gaetan Lepage <gaetan@glepage.com>
2025-09-30 00:36:54 +0200

    reinstall pascal with ZFS pool for /nix

--- hosts/pascal/default.nix
+++ hosts/pascal/default.nix
@@ -8,6 +8,9 @@
   hardware.nvidia.open = false;
   programs.nix-required-mounts.allowedPatterns.nvidia-gpu.onFeatures = [ "cuda-pascal" ];
 
-  networking.hostName = "pascal";
+  networking = {
+    hostName = "pascal";
+    hostId = "c0a6b9c4";
+  };
   system.stateVersion = "25.05";
 }
--- hosts/pascal/disko.nix
+++ hosts/pascal/disko.nix
@@ -1,27 +1,57 @@
 {
-  disko.devices.disk.ssd = {
-    type = "disk";
-    device = "/dev/disk/by-id/ata-Micron_1100_MTFDDAK512TBN_1707191CB464";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          size = "1M";
-          type = "EF02"; # for grub MBR
-          attributes = [ 0 ]; # partition attribute
-        };
-        swap = {
-          size = "64G";
-          content.type = "swap";
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
+  disko.devices = {
+    disk = {
+      ssd = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-Micron_1100_MTFDDAK512TBN_1707191CB464";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              attributes = [ 0 ]; # partition attribute
+            };
+            swap = {
+              size = "64G";
+              content.type = "swap";
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
         };
+      };
+
+      nix = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-Micron_1100_MTFDDAK512TBN_1707191CB452";
+        content = {
+          type = "gpt";
+          partitions.nix = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "znix";
+            };
+          };
+        };
+      };
+    };
+
+    zpool.znix = {
+      type = "zpool";
+      options = {
+        ashift = "9";
+      };
+      datasets.nix = {
+        type = "zfs_fs";
+        mountpoint = "/nix";
       };
     };
   };
