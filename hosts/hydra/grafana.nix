2cda65c359a430bcabdb2e603f098201ede673de
Gaetan Lepage <gaetan@glepage.com>
2025-10-29 14:19:12 +0100

    grafana: add prometheus

--- hosts/hydra/grafana.nix
+++ hosts/hydra/grafana.nix
@@ -17,6 +17,39 @@ in
           root_url = "https://${domain}";
         };
       };
+      provision = {
+        enable = true;
+        datasources.settings.datasources = [
+          {
+            name = "prometheus";
+            type = "prometheus";
+            url = "http://localhost:${toString config.services.prometheus.exporters.node.port}";
+          }
+        ];
+        dashboards = {
+        };
+      };
+    };
+    # Collect system metrics using prometheus and node exporter
+    prometheus = {
+      enable = true;
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+        };
+      };
+      scrapeConfigs = [
+        {
+          job_name = "node_exporter";
+          scrape_interval = "10s";
+          static_configs = [
+            {
+              targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+            }
+          ];
+        }
+      ];
     };
 
     caddy = {
