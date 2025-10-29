797a4fa657286e980b1f86a670f5b713420bc92f
Gaetan Lepage <gaetan@glepage.com>
2025-10-29 13:44:40 +0100

    grafana: add root_url

--- hosts/hydra/grafana.nix
+++ hosts/hydra/grafana.nix
@@ -1,18 +1,28 @@
 { config, ... }:
+let
+  domain = "grafana.nixos-cuda.org";
+in
 {
   services = {
     grafana = {
       enable = true;
 
       settings = {
-        server.http_port = 3001;
+        # Allow public access
+        "auth.anonymous".enabled = true;
+
+        server = {
+          http_port = 3001;
+
+          root_url = "https://${domain}";
+        };
       };
     };
 
     caddy = {
       enable = true;
 
-      virtualHosts."grafana.nixos-cuda.org".extraConfig = ''
+      virtualHosts.${domain}.extraConfig = ''
         reverse_proxy localhost:${toString config.services.grafana.settings.server.http_port}
       '';
     };
