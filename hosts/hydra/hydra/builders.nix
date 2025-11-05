8aa66de04591b2382c4d11fd6245e2c88a814067
Gaetan Lepage <gaetan@glepage.com>
2025-11-05 11:30:21 +0100

    hydra/builders: cleanup

--- hosts/hydra/hydra/builders.nix
+++ hosts/hydra/hydra/builders.nix
@@ -16,12 +16,9 @@
 
         baseDomain = "nixos-cuda.org";
       in
-      # 3 builders:
-      # - CPU builder: for building regular derivations (which do not require a GPU)
-      # - ada: for most cuda GPU tests
-      # - pascal: for cuda-pascal GPU tests (older GPU)
       [
-        # CPU builder
+        ########### CPU builders
+        # atlas
         {
           hostName = "atlas.${baseDomain}";
           sshUser = "nix";
@@ -30,7 +27,17 @@
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhwbG9zNlh0STY1VmVqbEMvdVNwbUF6bjJ4MEloZFIzYTl4d3ZFbWJsN0Igcm9vdEBhdGxhcwo=";
           maxJobs = 10;
         }
+        # oxide-1
+        {
+          hostName = "45.154.216.118";
+          sshUser = "nix";
+          inherit sshKey supportedFeatures system;
+          # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpPSWlZVThYWnU5NlJoWUZyUnoweVlOUEVnSDUxTTRjRHgrSW1YTHpSeDcgcm9vdEBveGlkZS0xCg==";
+          maxJobs = 3;
+        }
 
+        ########### GPU builders
         # Ada
         {
           hostName = "ada.${baseDomain}";
@@ -42,7 +49,7 @@
           maxJobs = 2;
         }
 
-        # Pascal
+        # pascal
         {
           hostName = "pascal.${baseDomain}";
           sshUser = "nix";
@@ -51,15 +58,6 @@
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUZqVGhLVXhhMDdjbDNtd1pIMnRJeHBDYmVkdG9IcFByK0Ntc1VZS1Z3M3Agcm9vdEBwYXNjYWwK";
           maxJobs = 2;
           mandatoryFeatures = [ "cuda-pascal" ];
-        }
-
-        {
-          hostName = "45.154.216.118";
-          sshUser = "nix";
-          inherit sshKey supportedFeatures system;
-          # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
-          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpPSWlZVThYWnU5NlJoWUZyUnoweVlOUEVnSDUxTTRjRHgrSW1YTHpSeDcgcm9vdEBveGlkZS0xCg==";
-          maxJobs = 3;
         }
       ];
   };
