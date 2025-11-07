30798eab5edeb8e6d82840bff55cd9fe085f24e5
Gaetan Lepage <gaetan@glepage.com>
2025-11-07 20:42:18 +0100

    dns: add github pages records for the website

--- dns.nix
+++ dns.nix
@@ -23,44 +23,61 @@
         registrar = "none";
         dnsProvider = "hetzner";
 
-        records = [
-          {
-            type = "a";
-            label = "ada";
-            address = "144.76.101.55";
-          }
-          {
-            type = "a";
-            label = "atlas";
-            address = "95.216.20.88";
-          }
-          {
-            type = "a";
-            label = "hydra";
-            address = "37.27.129.22";
-          }
-          {
-            type = "a";
-            label = "pascal";
-            address = "95.216.72.164";
-          }
-          {
-            type = "a";
-            label = "oxide-1";
-            address = "45.154.216.118";
-          }
+        records =
+          let
+            # https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site
+            githubPagesIps = [
+              "185.199.108.153"
+              "185.199.109.153"
+              "185.199.110.153"
+              "185.199.111.153"
+            ];
+            websiteRecords = map (address: {
+              type = "a";
+              label = "@";
+              inherit address;
+            }) githubPagesIps;
+          in
+          websiteRecords
+          ++ [
+            # servers
+            {
+              type = "a";
+              label = "ada";
+              address = "144.76.101.55";
+            }
+            {
+              type = "a";
+              label = "atlas";
+              address = "95.216.20.88";
+            }
+            {
+              type = "a";
+              label = "hydra";
+              address = "37.27.129.22";
+            }
+            {
+              type = "a";
+              label = "pascal";
+              address = "95.216.72.164";
+            }
+            {
+              type = "a";
+              label = "oxide-1";
+              address = "45.154.216.118";
+            }
 
-          {
-            type = "cname";
-            label = "cache";
-            target = "hydra";
-          }
-          {
-            type = "cname";
-            label = "grafana";
-            target = "hydra";
-          }
-        ];
+            {
+              type = "cname";
+              label = "cache";
+              target = "hydra";
+            }
+            {
+              type = "cname";
+              label = "grafana";
+              target = "hydra";
+            }
+          ];
       };
     };
   };
