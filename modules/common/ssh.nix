1e88f1509c93412931bd920bd9fe60fd75e8684a
Gaetan Lepage <gaetan@glepage.com>
2025-12-03 11:38:18 +0100

    Remove root access to Serge

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -9,9 +9,6 @@
       authorizedKeys = [
         # @GaetanLepage
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJSonNBBb1DlhaO4EfMh3TbIIsV25phZQ9vp/qKOw9E"
-
-        # @SomeoneSerge
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILQBX9vypn+dy8nqtV5cchWes2xB5MqsBVMrtJ6hjX1D"
       ];
     in
     {
@@ -20,6 +17,9 @@
       nix.openssh.authorizedKeys.keys = [
         # Hydra's SSH key
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDi1X4YCtWEto02ovI/fsond7hMKPZ0cFYMLkGn9rGtu"
+
+        # @SomeoneSerge
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILQBX9vypn+dy8nqtV5cchWes2xB5MqsBVMrtJ6hjX1D"
       ]
       ++ authorizedKeys;
     };
