12bce4b5bf8aa81d265d42df85250ef0b46c6243
Gaetan Lepage <gaetan@glepage.com>
2026-01-22 18:57:24 +0100

    monitoring: simpler way to open prometheus node exporter TCP port

--- modules/common/monitoring.nix
+++ modules/common/monitoring.nix
@@ -1,11 +1,8 @@
-{ config, ... }:
 {
   services.prometheus.exporters.node = {
     enable = true;
     enabledCollectors = [ "systemd" ];
-  };
 
-  networking.firewall.allowedTCPPorts = [
-    config.services.prometheus.exporters.node.port
-  ];
+    openFirewall = true;
+  };
 }
