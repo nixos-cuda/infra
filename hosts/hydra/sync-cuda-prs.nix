c6d9bf7b4521541786e9afb2654a712bc776cd44
Yuriy Taraday <yuriy.taraday@tweag.io>
2026-05-12 15:57:06 +0200

    hydra: Sync PRs from NixOS/nixpkgs to nixos-cuda/nixpkgs
    Every 5 minutes scan last 100 Nixpkgs PRs, find ones created by a member
    of CUDA team and mirror it to nixos-cuda/nixpkgs. If PR has already been
    mirrored, then stop.

---
+++ hosts/hydra/sync-cuda-prs.nix
@@ -0,0 +1,69 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  users = [
+    "ConnorBaker"
+    "GaetanLepage"
+    "SomeoneSerge"
+    "YorikSar"
+  ];
+  serviceName = "sync-cuda-prs";
+  helpers = pkgs.lib.getExe inputs.self.packages.${config.nixpkgs.system}.helpers;
+  script = lib.concatStringsSep " " (
+    [
+      helpers
+
+      # channel-updater GitHub app client ID
+      "--client-id"
+      "Iv23liZJuJFjr3N1KsP0"
+
+      # Path to the channel-updater Github app key file
+      "--private-key"
+      "$CREDENTIALS_DIRECTORY/github_app_key"
+
+      "sync-prs"
+
+      # from upstream repo
+      "--upstream-repo-full-name"
+      "NixOS/nixpkgs"
+
+      # to nixpkgs fork
+      "--repo-full-name"
+      "nixos-cuda/nixpkgs"
+
+      # body for new PRs
+      "--body"
+      "Clone of {html_url} for CI"
+
+      # scan last 100 PRs
+      "--top"
+      "100"
+    ]
+    ++ (lib.concatMap (user: [
+      "--from-users"
+      user
+    ]) users)
+  );
+in
+{
+  systemd.timers.${serviceName} = {
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnCalendar = "*:1/5"; # every 5 minutes
+    timerConfig.Unit = "${serviceName}.service";
+  };
+  systemd.services.${serviceName} = {
+    inherit script;
+    serviceConfig = {
+      Type = "oneshot";
+      DynamicUser = true;
+      LoadCredential = [
+        "github_app_key:${config.sops.secrets.channel-updater-github-app-key.path}"
+      ];
+    };
+  };
+}
