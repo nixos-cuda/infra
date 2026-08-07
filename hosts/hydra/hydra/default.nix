335dde8faf1def1647722b7e8c0f200270213e34
Gaetan Lepage <gaetan@glepage.com>
2026-08-07 10:53:55 +0200

    hydra: remove redundant zstd from hydra-server path

--- hosts/hydra/hydra/default.nix
+++ hosts/hydra/hydra/default.nix
@@ -103,9 +103,6 @@ in
       };
     };
 
-  # hydra-server uses zstd binary to serve logs when they are compressed with zstd
-  systemd.services.hydra-server.path = [ pkgs.zstd ];
-
   networking.firewall.allowedTCPPorts = [
     80
     443
